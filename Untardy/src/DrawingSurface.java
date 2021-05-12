32a58a0b8425a6db833db9fe4131785c1271c93f
jerrywusa <69756901+jerrywusa@users.noreply.github.com>
2021-05-12 16:01:04 -0700

    Revert "added player movement"
    This reverts commit a32b77f968bd3f1a922e2b7911f61534a09b115a.

--- Untardy/src/DrawingSurface.java
+++ Untardy/src/DrawingSurface.java
@@ -1,6 +1,4 @@
 
-import java.awt.event.KeyEvent;
-
 import processing.core.PApplet;
 
 /**
@@ -11,14 +9,14 @@ import processing.core.PApplet;
 public class DrawingSurface extends PApplet {
 
     private Grid grid;
-
-    public int time;
-    public static final int wait = 100;
-
+    
+  	public int time;
+  	public static final int wait = 100;
+  	
     private boolean intro;
     private boolean tardy;
     private boolean finish;
-
+    
     private PlayerData playerData;
     private Clock clock;
 
@@ -30,57 +28,47 @@ public class DrawingSurface extends PApplet {
         playerData = new PlayerData();
         clock = new Clock(12, 58, "AM");
     }
-
+    
     public void setup() {
-        time = millis();
-    }
+		time = millis();
+	}
 
     public void draw() {
-
-        background(255);
-        fill(0);
-
-        if (intro) {
-            // Display introduction screen until clicked off of
-        }
-
-        if (tardy) {
-            // Pop up telling player they got a tardy, then restarts the day
-        }
-
-        if (finish) {
-            // Display victory screen with player score
-        }
-
-        // Display regular game
-        if (grid != null) {
-            grid.draw(this, 0, 0, height, height);
-        }
-        text(clock.displayTime(), width * 3 / 4, height * 1 / 8);
-        text(playerData.printSchedule(), width * 3 / 4, height * 3 / 8);
-
-        // time updates with every second, use with clock methods to update clock
-        if (millis() - time >= wait) {
-            clock.updateTime();
-            time = millis();
-        }
+    	
+    	background(255);
+    	fill(0);
+    	
+    	if(intro) {
+    		//Display introduction screen until clicked off of
+    	}
+    	
+    	if(tardy) {
+    		//Pop up telling player they got a tardy, then restarts the day
+    	}
+    	
+    	if(finish) {
+    		//Display victory screen with player score
+    	}
+    	
+    	//Display regular game
+    	if(grid != null) {
+    		grid.draw(this, 0, 0, height, height);
+    	}
+        text(clock.displayTime(), width*3/4, height*1/8);
+        text(playerData.printSchedule(), width*3/4, height*3/8);
+        
+        //time updates with every second, use with clock methods to update clock
+  		if(millis() - time >= wait) {
+  			clock.updateTime();
+  			time = millis();
+  		}
     }
-
-    public void keyPressed() {
-        // call grid function to move players based on keyPress
-        switch (keyCode) {
-            case keyEvent.VK_UP:
-                grid.movePlayerUp();
-                break;
-            case keyEvent.VK_DOWN:
-                grid.movePlayerDown();
-                break;
-            case KeyEvent.VK_LEFT:
-                grid.movePlayerLeft();
-                break;
-            case KeyEvent.VK_RIGHT:
-                grid.movePlayerRight();
-                break;
-        }
+    
+    public void mousePressed() {
+    	if(mouseButton == LEFT) {
+    		if(intro) { //Click anywhere to exit introduction screen
+    			intro = false;
+        	}
+    	}
     }
 }
--- Untardy/src/Grid.java
+++ Untardy/src/Grid.java
@@ -13,11 +13,9 @@ public class Grid {
 
     private Cell[][] grid;
     private int[] playerLocation;
-    private Cell cellUnderPlayer;
 
     public Grid() {
         this.grid = new Cell[20][20];
-        this.cellUnderPlayer = new Cell();
         this.playerLocation = new int[2];
         Arrays.fill(playerLocation, -1);
         for (int r = 0; r < grid.length; r++) {
@@ -36,15 +34,15 @@ public class Grid {
 
     public Grid(int height, int width, String filename) {
         grid = new Cell[height][width];
-        this.cellUnderPlayer = new Cell();
+        
         this.readData(filename, grid);
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (grid[i][j] == null) {
-                    grid[i][j] = new Cell();
-                }
-
-            }
+        for(int i=0; i<height; i++) {
+        	for(int j=0; j<width; j++) {
+        		if(grid[i][j] == null) {
+        			grid[i][j] = new Cell();
+        		}
+        		
+        	}
         }
         this.playerLocation = new int[2];
         Arrays.fill(playerLocation, -1);
@@ -79,7 +77,7 @@ public class Grid {
                 marker.fill(grid[r][c].fillColor()[0], grid[r][c].fillColor()[1], grid[r][c].fillColor()[2]);
                 marker.rect(rectX, rectY, rectWidth, rectHeight);
                 marker.popStyle();
-
+                
             }
         }
     }
@@ -141,70 +139,6 @@ public class Grid {
         return true;
     }
 
-    public void movePlayerUp() {
-        int r = playerLocation[0];
-        int c = playerLocation[1];
-        int[] dir = { -1, 0 };
-        int nr = r + dir[0];
-        int nc = c + dir[1];
-        if (inBounds(grid, nr, nc) && grid[nr][nc].isTraversable()) {
-            Cell playerCell = grid[r][c];
-            grid[r][c] = cellUnderPlayer;
-            cellUnderPlayer = grid[nr][nc];
-            grid[nr][nc] = playerCell;
-            playerLocation[0] = nr;
-            playerLocation[1] = nc;
-        }
-    }
-
-    public void movePlayerDown() {
-        int r = playerLocation[0];
-        int c = playerLocation[1];
-        int[] dir = { 1, 0 };
-        int nr = r + dir[0];
-        int nc = c + dir[1];
-        if (inBounds(grid, nr, nc) && grid[nr][nc].isTraversable()) {
-            Cell playerCell = grid[r][c];
-            grid[r][c] = cellUnderPlayer;
-            cellUnderPlayer = grid[nr][nc];
-            grid[nr][nc] = playerCell;
-            playerLocation[0] = nr;
-            playerLocation[1] = nc;
-        }
-    }
-
-    public void movePlayerLeft() {
-        int r = playerLocation[0];
-        int c = playerLocation[1];
-        int[] dir = { 0, -1 };
-        int nr = r + dir[0];
-        int nc = c + dir[1];
-        if (inBounds(grid, nr, nc) && grid[nr][nc].isTraversable()) {
-            Cell playerCell = grid[r][c];
-            grid[r][c] = cellUnderPlayer;
-            cellUnderPlayer = grid[nr][nc];
-            grid[nr][nc] = playerCell;
-            playerLocation[0] = nr;
-            playerLocation[1] = nc;
-        }
-    }
-
-    public void movePlayerRight() {
-        int r = playerLocation[0];
-        int c = playerLocation[1];
-        int[] dir = { 0, 1 };
-        int nr = r + dir[0];
-        int nc = c + dir[1];
-        if (inBounds(grid, nr, nc) && grid[nr][nc].isTraversable()) {
-            Cell playerCell = grid[r][c];
-            grid[r][c] = cellUnderPlayer;
-            cellUnderPlayer = grid[nr][nc];
-            grid[nr][nc] = playerCell;
-            playerLocation[0] = nr;
-            playerLocation[1] = nc;
-        }
-    }
-
     // Method used from GridTemplate.java in Recursion2DArrays lab
     public void readData(String filename, Cell[][] gameData) {
         File dataFile = new File(filename);
@@ -222,18 +156,23 @@ public class Grid {
                     String line = in.nextLine();
                     for (int i = 0; i < line.length(); i++)
                         if (count < gameData.length && i < gameData[count].length) {
-                            if (line.charAt(i) == 'P') { // Player
-                                gameData[count][i] = new PlayerCell();
-                            } else if (line.charAt(i) == '*') { // Building
-                                gameData[count][i] = new BuildingCell();
+                        	if(line.charAt(i) == 'P') { //Player
+                        		System.out.println("test");
+                        		gameData[count][i] = new PlayerCell();
+                        	}else if(line.charAt(i) == '*') { //Building
+                        		System.out.println("test");
+                        		gameData[count][i] = new BuildingCell();
 
-                            } else if (line.charAt(i) == ' ') { // Path
-                                gameData[count][i] = new PathCell();
-                            } else if (line.charAt(i) == '.') { // Vegetation
-                                gameData[count][i] = new VegetationCell();
-                            } else {
-                                gameData[count][i] = new Cell();
-                            }
+                        	}else if(line.charAt(i) == ' ') { //Path
+                        		System.out.println("test");
+                        		gameData[count][i] = new PathCell();
+
+                        	}else if(line.charAt(i) == '.') { //Vegetation
+                        		gameData[count][i] = new VegetationCell();
+
+                        	}else {
+                        		gameData[count][i] = new Cell();
+                        	}
                         }
                     count++;
 
