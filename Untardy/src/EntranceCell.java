2075be3807dbdca408cf6fee2b3927063924644b
jerrywusa <69756901+jerrywusa@users.noreply.github.com>
2021-05-12 23:16:25 -0700

    made javadoc for all cell classes

--- Untardy/src/EntranceCell.java
+++ Untardy/src/EntranceCell.java
@@ -1,20 +1,30 @@
-
+/**
+ * 
+ * @author Jerry Wu
+ *
+ */
 public class EntranceCell extends Cell {
 	private char buildingChar;
 	
 	/**
 	 * creates new EntranceCell
-	 * @param the
+	 * @param buildingChar character name of building
 	 */
 	public EntranceCell(char buildingChar) {
 		this.isTraversable = true;
 		this.buildingChar = buildingChar;
 	}
 	
+    /**
+     * @return array of length 3 which corresponds with RGB value of Cell color
+     */
 	public int[] fillColor() {
 		return new int[] {102, 51, 0};
 	}
 	
+    /**
+     * @return building's char that this Cell corrosponds with
+     */
 	public char getBuildingChar() {
 		return buildingChar;
 	}
